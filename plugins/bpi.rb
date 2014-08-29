dff6ac9fa233d9371ed2c30abe43dc30cbfc7eda
Jordan Bouellat <j.bouellat@gmail.com>
2014-08-29 14:20:46 +0200

    bpi plugin

---
+++ plugins/bpi.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+
+module Slacker
+
+  class BPI< Plugin
+    def help
+      'Usage: slacker bpi -> slackers returns the current Bitcoin Price Index from www.coindesk.com'
+    end
+
+    def pattern
+      /bpi/
+    end
+
+    def respond
+      url = 'http://api.coindesk.com/v1/bpi/currentprice.json'
+      resp = Net::HTTP.get_response(URI.parse(url))
+
+      data = JSON.parse(resp.body)
+      return data['bpi']['USD']['rate']+"USD"
+    end
+
+    Bot.register(BPI)
+
+  end
+end
